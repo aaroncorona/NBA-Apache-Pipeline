efc440d1050eee9c13ec689ad2fdc1b63e4775cb
Aaron Corona <31792170+aaroncorona@users.noreply.github.com>
2024-12-29 12:32:59 -0800

    Add salary details by team

--- NbaCsvProcessor.scala
+++ NbaCsvProcessor.scala
@@ -14,12 +14,13 @@ object NbaCsvProcessor {
     val delimiter = ","
     
     // Load CSV file into a DataFrame
-    val df = sparkSession.read
+    val dfRaw = sparkSession.read
       .format(fileType)
       .option("inferSchema", inferSchema)
       .option("header", firstRowIsHeader)
       .option("sep", delimiter)
       .load(fileLocation)
+    val df = dfRaw.withColumn("Salary", col("Salary").cast("double"))
     println("Raw data:")
     df.show(5)
 
@@ -30,5 +31,19 @@ object NbaCsvProcessor {
       .agg(round(avg("Age"), 2).alias("Average Age"), max("Age").alias("Max Age"))
       println("Ages detail by Team:")
       playerAges.show(5);
+    
+    // Transform to an RDD for more low level control for processing salaries
+    val rdd: RDD[(String, Double)] = df.rdd.map(row => (row.getAs[String]("Team"), row.getAs[Double]("Salary")))
+    val aggregatedRdd: RDD[(String, (Double, Int))] = rdd.mapValues(salary => (salary, 1))
+      .reduceByKey((acc, value) => (acc._1 + value._1, acc._2 + value._2)) // Sum up salaries and count players
+
+    // Show the salary details per team
+    val avgSalaryRdd: RDD[(String, Double, Double)] = aggregatedRdd.map {
+      case (team, (totalSalary, playerCount)) => (team, totalSalary, totalSalary / playerCount)
+    }
+    println("Salary detail by Team:")
+    avgSalaryRdd.collect().foreach {
+      case (team, totalSalary, avgSalary) => println(s"Team: $team, Total Salary: $$${"%.2f".format(totalSalary)}, Average Salary: $$${"%.2f".format(avgSalary)}")
+    }
   }
 }
