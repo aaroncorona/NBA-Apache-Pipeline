2258e771e0156b824d30cbabe886857461aeb331
Aaron Corona <31792170+aaroncorona@users.noreply.github.com>
2024-12-30 13:19:18 -0800

    Update the DAG to use BashOperator to trigger the pipeline

--- dags/nba_pipeline_dag.py
+++ dags/nba_pipeline_dag.py
@@ -1,28 +1,25 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
-default_args = {
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-}
-
-# This can be run locally via http://localhost:8080/dags/nba_pipeline_dag
-with DAG(
-    dag_id='nba_pipeline_dag',
-    default_args=default_args,
-    schedule_interval='@daily',
+# Define the DAG
+dag = DAG(
+    'nba_spark_pipeline',
+    description='A simple Spark job for NBA processing',
+    schedule_interval='@daily',  # Set the frequency for running the DAG
+    start_date=datetime(2024, 12, 30),
     catchup=False,
-) as dag:
+)
 
-    task_1 = BashOperator(
-        task_id='print_date',
-        bash_command='date',
-    )
+# Define the Spark submit command
+jar_path = '/Users/aaroncorona/airflow/pipelines/project/src/main/scala/target/scala-2.12/nba-spark-pipeline_2.12-0.1.jar'
+spark_submit_command = f"""
+spark-submit --class Main --master local[*] {jar_path}
+"""
 
-    task_2 = BashOperator(
-        task_id='say_hello',
-        bash_command='echo "Hello, Airflow!"',
-    )
-
-    task_1 >> task_2
+# Create a BashOperator task to execute the spark-submit command
+run_spark_job = BashOperator(
+    task_id='run_spark_job',
+    bash_command=spark_submit_command,
+    dag=dag,
+)
