b576577f19dd2dde439815bcc3ad143f33af2315
Aaron Corona <31792170+aaroncorona@users.noreply.github.com>
2025-01-02 10:40:36 -0800

    Add Kafka Producer streaming after the initial Spark batch job

--- pipelines/project/src/main/scala/Main.scala
+++ pipelines/project/src/main/scala/Main.scala
@@ -8,6 +8,13 @@ object Main {
       .master("local[*]") // Use all available cores on the local machine
       .getOrCreate()
 
+    // Start the Spark batch job to process the CSV
     NbaCsvProcessor.loadCSV(sparkSession)
+
+    // Send some of the data over a Kafka stream
+    val kafkaTopic = "nba-salary-topic"
+    KafkaProducerService.produceData("OKC", "Russell Westbrook, 25, $10,000,000", kafkaTopic)
+    KafkaProducerService.produceData("LAL", "Kobe Bryant, 35, $25,000,000", kafkaTopic)
+    KafkaProducerService.closeProducer()
   }
 }
